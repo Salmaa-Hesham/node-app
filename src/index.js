c8ec7a031100c36f2e260dc08e263e8167b9261c
Salma Hesham <salmahesham1114@gmail.com>
2024-12-11 03:14:02 +0200

    The source code of the app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const PORT = 4000; 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1> Hello world hi hi </h1>'));
+app.get('/', (req, res) => res.send('<h1> Hello world hi hi</h1>'));
 
 app.listen(PORT, () => console.log(`app is up and running on port: ${PORT}`));
